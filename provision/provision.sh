46a4ac8bd16815c61cadecee668084f02056ced1
Aleksandar Djurdjic <al.djurdjic@gmail.com>
2014-01-12 19:47:23 +0100

    added mysql init to provision

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-apt-get update >/dev/null 2>&1
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y --force-yes apache2 mysql-server-5.5 libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt >/dev/null 2>&1
+apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y --force-yes apache2 mysql-server-5.5 libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt
 
-mysql -uroot -e "UPDATE mysql.user SET password=PASSWORD('') WHERE user='root'; FLUSH PRIVILEGES;"; >/dev/null
+mysql -uroot -e "UPDATE mysql.user SET password=PASSWORD('') WHERE user='root'; FLUSH PRIVILEGES;"
+mysql -uroot -e "CREATE DATABASE mydb;"
+mysql -uroot mydb < /vagrant/mydb_2014-01-12.sql
 
 rm -fr /var/www
 ln -fs /vagrant /var/www
 
-service mysql restart >/dev/null
-service apache2 restart >/dev/null
+service mysql restart
+service apache2 restart
